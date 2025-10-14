1e355078101b08817a1a17b80197213f66629d66
SarahABusch <renata_gomes88@hotmail.com>
2025-10-14 00:17:50 -0300

    começo e fim desse prijeto

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+import {persons} from "./persons"
+
+const app = express()
+const port = 3333
+
+app.use(cors())
+app.get("/", (request, response) => {
+    response.json(persons)
+} )
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}!`)
+})
---
+++ src/persons.js
@@ -0,0 +1,31 @@
+const persons = [
+    {
+        name: "Sarah Busch",
+        nickname: "Sasah",
+        age: 17,
+        email: "busch@gmail.com"
+    },
+
+     {
+        name: "Sarah Andrade",
+        nickname: "Taiyo",
+        age: 16,
+        email: "andrade@gmail.com"
+    },
+
+     {
+        name: "Sarah Andrade Busch",
+        nickname: "Yasu",
+        age: 15,
+        email: "andradebusch@gmail.com"
+    },
+
+     {
+        name: "Gabby",
+        nickname: "BIBI",
+        age: 16,
+        email: "contapessoaldagabby@gmail.com"
+    },
+]
+
+export {persons}
